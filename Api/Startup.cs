8b23003881db83e39fbed66914f4adcf172ddea9
Saba Gotsiridze <s.gotsiridze@altasoft.ge>
2022-09-18 18:30:38 +0400

    ref

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,7 +1,9 @@
+using Api.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,12 +18,12 @@ namespace Api
 {
 	public class Startup
 	{
+		private readonly IConfiguration _configuration;
+
 		public Startup(IConfiguration configuration)
 		{
-			Configuration = configuration;
+			_configuration = configuration;
 		}
-
-		public IConfiguration Configuration { get; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
@@ -30,6 +32,10 @@ namespace Api
 			services.AddSwaggerGen(c =>
 			{
 				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
+			});
+			services.AddDbContext<StoreContext>(ops =>
+			{
+				ops.UseSqlServer(_configuration.GetConnectionString("DefaultConnectionString"));
 			});
 		}
 
