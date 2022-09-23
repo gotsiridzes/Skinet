ffcfa5198ac57ec9ec891d41d094b780bbcd4d93
Saba Gotsiridze <s.gotsiridze@altasoft.ge>
2022-09-23 18:00:51 +0400

    pagination class

---
+++ Api/Helpers/Pagination.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Api.Helpers
+{
+	public class Pagination<T> where T : class
+	{
+		public int PageIndex { get; set; }
+		public int PageSize { get; set; }
+		public int Count { get; set; }
+		public IReadOnlyList<T> Data { get; set; }
+	}
+}
