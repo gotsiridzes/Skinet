4b9dd409eee637974f06fa6392ae3f9ba5deb76d
Saba Gotsiridze <s.gotsiridze@altasoft.ge>
2022-09-23 17:40:30 +0400

    specification mod

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -25,6 +25,9 @@ namespace Infrastructure.Data
 			if (specification.OrderByDesc != null)
 				query = query.OrderByDescending(specification.OrderByDesc);
 
+			if (specification.IsPagingEnabled)
+				query = query.Skip(specification.Skip).Take(specification.Take);
+		
 			query = specification.Includes.Aggregate(query, (current, include) => current.Include(include));
 
 			return query;
