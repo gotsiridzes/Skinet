bc63415dbd73db2b586979f211dba41cbafb3097
Saba Gotsiridze <s.gotsiridze@altasoft.ge>
2022-12-05 18:01:23 +0400

    Add Pagination and product models.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from "./models/product";
+import { IPagination } from "./models/pagination";
 
 @Component({
   selector: 'app-root',
@@ -8,15 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: any[];
+  products: IProduct[];
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
-      this.products = response.data;
-      console.log(response);
-    }, error => {
-      console.log(error);
-    });
+    this.http
+      .get('https://localhost:5001/api/products?pageSize=50')
+      .subscribe((response: IPagination) => {
+        this.products = response.data;
+        console.log(response);
+      }, error => {
+        console.log(error);
+      });
   }
 }
